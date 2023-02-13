95e2d0de629ca8e0723c9f968164ffec1bf4de39
ibrahimmiah <ibrahimmiah787@gmail.com>
2023-02-13 00:21:59 -0500

    Failed to merge

--- src/test/java/com/learn/test/MyName.java
+++ src/test/java/com/learn/test/MyName.java
@@ -3,7 +3,9 @@ package com.learn.test;
 public class MyName {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Ibrahim Miah");
+		System.out.println("Hi This is for test");
+		System.out.println("I am from Bangladesh");
+		System.out.println("New Line added");
 		
 	}
 
